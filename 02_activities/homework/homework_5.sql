81db6b8bc2512c3564790d550678d6300018aa69
kamanddst <kamanddst@gmail.com>
2024-09-21 23:46:15 -0400

    homework_6 done.

--- 02_activities/homework/homework_5.sql
+++ 02_activities/homework/homework_5.sql
@@ -9,19 +9,59 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+	WITH VendorProducts AS (
+    SELECT  
+		v.vendor_name,
+		vi.original_price,
+		vi.quantity,
+		p.product_name
+	  FROM vendor as v
+	JOIN vendor_inventory as vi
+	on v.vendor_id = vi.vendor_id
+	JOIN product as p
+	on vi.product_id = p.product_id
+),
+CustomerCount AS (
+    SELECT COUNT(DISTINCT customer_id) AS total_customers
+    FROM customer_purchases
+)
 
+SELECT 
+    vp.vendor_name,
+    vp.product_name,
+    (5 * cc.total_customers * vp.original_price ) AS total_revenue
+FROM 
+    VendorProducts AS vp
+CROSS JOIN 
+    CustomerCount AS cc
+GROUP by vp.product_name, vp.vendor_name
+
+
+
+	
 
 -- INSERT
 /*1.  Create a new table "product_units". 
 This table will contain only products where the `product_qty_type = 'unit'`. 
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
-
-
+DROP TABLE IF EXISTS product_units;
+CREATE TEMP TABLE product_units AS 
+SELECT 
+    *,
+    CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM 
+    product
+WHERE 
+    product_qty_type = 'unit';
+	
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+	
+INSERT  INTO product_units 
+VALUES(101, 'ApplePie', '10"', 3,'unit',CURRENT_TIMESTAMP);
 
 
 -- DELETE
@@ -30,6 +70,10 @@ This can be any product you desire (e.g. add another record for Apple Pie). */
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
 
+DELETE FROM product_units
+WHERE 
+    product_name = 'Apple Pie'
+    AND snapshot_timestamp < CURRENT_TIMESTAMP;
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -47,5 +91,26 @@ Third, SET current_quantity = (...your select statement...), remembering that WH
 Finally, make sure you have a WHERE statement to update the right row, 
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
+
+
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+WITH last_quantity As(
+SELECT 
+product_id,
+market_date, 
+quantity,
+ROW_NUMBER() OVER (PARTITION BY product_id ORDER by market_date DESC) as latest_quantity
+FROM vendor_inventory as v
+)
+
+UPDATE product_units 
+Set current_quantity = coalesce((
+SELECT 
+lq.quantity
+FROM last_quantity as lq
+WHERE lq.product_id = product_units.product_id and latest_quantity = 1)
+,0);
 
 
